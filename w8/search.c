18503bb1c39c4a62b8b0565c114374e698d33da6
Mashed <leventezoltan@protonmail.com>
2025-09-16 19:06:09 +1000

    modified serial code to be simpler

--- w8/search.c
+++ w8/search.c
@@ -1,14 +1,11 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <math.h>
-#include <mpi.h>
+
+int is_prime(int n);
 
 int main(int argc, char *argv[])
 {
-    MPI_Init(&argc, &argv);
-    int rank, size;
-    MPI_Comm_rank(MPI_COMM_WORLD, &rank); // process ID
-    MPI_Comm_size(MPI_COMM_WORLD, &size); // total number of processes
 
     if (argc != 2)
     {
@@ -16,58 +13,44 @@ int main(int argc, char *argv[])
         return 1;
     }
 
-    int input = atoi(argv[1]); // convert argument to integer
-    if (input <= 0)
+    int n = atoi(argv[1]); // convert argument to integer
+    if (n <= 0)
     {
         printf("Please enter a positive integer.\n");
         return 1;
     }
 
-    int remainder;
     // Main loop
-    for (int prime = 2, counter = 0, divider = 2, composite = 0; counter < input; prime += 2)
+    int count = 0;
+
+    for (int i = 2; i < n; i++)
     {
-        // This offsets the increments, so that it skips every even number after 2, because 2 is the only even prime number
-        if (prime == 4)
+        if (is_prime(i))
         {
-            prime--;
+            count++;
+            printf("%d ", i);
         }
-
-        // This loop is used to check if the prime variable is actually a prime number
-        do
-        {
-            remainder = prime % divider;
-
-            if (divider < prime && remainder == 0)
-            {
-                composite = 1;
-            }
-
-            if (divider > sqrt(prime) && remainder != 0)
-            {
-                remainder = 0;
-                divider = prime;
-            }
-
-            if (remainder != 0)
-            {
-                divider++;
-            }
-
-        }
-        while ((remainder != 0 || prime != divider) && composite != 1);
-
-        composite = 0;
-
-        if (prime == divider && remainder == 0)
-        {
-            printf("%i ", prime);
-            counter++;
-        }
-
-        divider = 2;
     }
 
     printf("\n");
     return 0;
+}
+
+// Simple prime checker
+int is_prime(int n)
+{
+    if (n < 2)
+        return 0;
+    if (n == 2)
+        return 1;
+    if (n % 2 == 0)
+        return 0;
+
+    int sqrt_n = (int)sqrt(n);
+    for (int i = 3; i <= sqrt_n; i += 2)
+    {
+        if (n % i == 0)
+            return 0;
+    }
+    return 1;
 }
